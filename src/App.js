3c7b3b9390268535274d4711a6ca9ab2dc3b3d6e
valmy <valmyericles@gmail.com>
2020-10-23 17:37:56 -0300

    ratings ok

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment, useState, useEffect } from 'react';
+import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+import EstablishmentService from './services/establishment_service';
+import Establishment from './components/Establishment';
 
 function App() {
+  const [latitude, setLatitude] = useState(0)
+  const [longitude, setLongitude] = useState(0)
+  const [locations, setLocations] = useState([])
+  const [selected, setSelected] = useState({})
+
+  const { REACT_APP_GOOGLE_KEY } = process.env;
+
+  useEffect(() => {
+    setCurrentLocation()
+  }, [])
+
+  useEffect(() => {
+    (latitude !== 0 && longitude !== 0) && loadCoffeShops()
+  }, [latitude, longitude]);
+
+  async function setCurrentLocation() {
+    await navigator.geolocation.getCurrentPosition(function(position) {
+      setLatitude(position.coords.latitude)
+      setLongitude(position.coords.longitude)
+    }, function(err) {
+      alert("Habilite a localização para usar esse APP")
+    })
+  }
+
+  async function loadCoffeShops() {
+    const response = await EstablishmentService.index(latitude, longitude);
+    setLocations(response.data.results);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Fragment>
+      <LoadScript googleMapsApiKey={REACT_APP_GOOGLE_KEY}>
+        <GoogleMap 
+          mapContainerStyle={{ height: "100vh", width: "100%" }}
+          zoom={15}
+          center={{ lat: latitude, lng: longitude }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          {
+            locations.map((item, index) => {
+              return (
+                <Marker 
+                  key={index}
+                  icon="/images/coffee-pin.png"
+                  title={item.name} 
+                  animation="4" 
+                  position={{lat: item.geometry.location.lat, lng: item.geometry.location.lng}}
+                  onClick={() => setSelected(item)}
+                />
+              )
+            })
+          }
+
+          {
+            selected.place_id && (
+              <Establishment place={selected} />
+            )
+          }
+
+          <Marker 
+            key="my location" 
+            icon="./images/my-location-pin.png"
+            title="Seu local" 
+            animation="2" 
+            position={{ lat: latitude, lng: longitude }}
+          />
+        </GoogleMap>
+      </LoadScript>
+    </Fragment>
   );
 }
 
